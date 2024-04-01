c573dcb9e2862972fd47ab133f1b5d37dcaf1f3b
Daniil Kirichenco <mannol.2015@mail.ru>
2024-04-01 15:12:04 +0300

    numbers chenged

--- src/Main.java
+++ src/Main.java
@@ -5,12 +5,12 @@ public class Main {
     public static void main (String[] args ) {
         Printer printer = new Printer();
         printer.append("some text 133","Справка");
-        printer.append("some text 210", 544);
+        printer.append("some text 210", 658);
         System.out.println(printer.getPagesCount());
         printer.print();
         printer.append("some text 8",530);
         printer.clear();
-        printer.append("some text 23", "Договор", 312);
+        printer.append("some text 33", "Договор", 312);
         printer.append("some text 11");
         printer.print();
         System.out.println(printer.getAllPagesPrinted());
